a736d70c8d1748104d0a3f5fbc078364c736ad5f
Zanirina <beketovazarina.bz@gmail.com>
2024-05-02 17:24:35 +0500

    Myhashtable_please_dont_cry

---
+++ src/MyHashTable.java
@@ -0,0 +1,134 @@
+public class MyHashTable<K, V> {
+    private int M = 11;
+    private double loadFactor = 0.25;
+    private HashNode<K, V>[] buckets;
+    private int size = 0;
+
+    public MyHashTable() {
+        buckets = new HashNode[M];
+    }
+
+    public MyHashTable(int initialCapacity) {
+        M = (int) (loadFactor * initialCapacity);
+        buckets = new HashNode[M];
+    }
+
+    public int hashToIndex(K key) {
+
+        return (key.hashCode() & 0xfffffff) % M;
+    }
+
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    public int countElements(int index){
+        int count = 0;
+        HashNode<K, V> temp = buckets[index];
+        while(temp != null){
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+    public int getM(){
+        return M;
+    }
+
+
+    public V get(K key) {
+        int index = hashToIndex(key);
+        HashNode<?, ?> temp = buckets[index];
+        while (temp.next != null) {
+            if (temp.key.equals(key)) {
+                return (V) temp.value;
+            }
+            temp = temp.next;
+        }
+        return null;
+    }
+
+    public boolean contains(K key){
+        return get(key)!=null;
+    }
+
+    public K getkey(V value){
+        for(int i=0; i<M; i++){
+            HashNode <?,?> temp = buckets[i];
+            while (temp.next!=null){
+                if(temp.value.equals(value)){
+                    return (K) temp.key;
+                }
+                temp = temp.next;
+            }
+
+        }
+        return null;
+    }
+
+
+    public void put(K key, V value){
+        if((double) size / M > loadFactor) increaseCapacity();
+        int index = hashToIndex(key);
+        HashNode<K ,V> a = new HashNode<>(key , value);
+
+        a.next = buckets[index];
+        buckets[index] = a;
+        size++;
+
+    }
+    public void reput(K key, V value){
+        int index = hashToIndex(key);
+        HashNode<K ,V> a = new HashNode<>(key , value);
+
+        a.next = buckets[index];
+        buckets[index] = a;
+
+    }
+    private void increaseCapacity(){
+        M = M*2;
+        HashNode<K,V> []temp = buckets;
+        buckets = new HashNode[M];
+        for(int i=0;i<temp.length;i++){
+            if(temp[i]!=null){
+                HashNode<K,V> node = temp[i];
+                while (node!=null){
+                    reput(node.key, node.value);
+                    node = node.next;
+                }
+            }
+
+        }
+
+    }
+    public V remove(K key){
+        int index = hashToIndex(key);
+        HashNode<K,V> temp = buckets[index];
+        HashNode<K,V> prev = null;
+        while (temp.next!=null){
+            if(temp.key.equals(key)){
+                if(prev==null){
+                    buckets[index] = temp.next;
+                }
+                else {
+                    prev.next = temp.next;
+                }
+                size--;
+                return temp.value;
+
+            }
+            prev = temp;
+            temp = temp.next;
+        }
+
+        return null;
+    }
+
+
+}
---
+++ src/MyTest.java
@@ -0,0 +1,21 @@
+public class MyTest {
+    String s;
+    String s2;
+
+    public MyTest(String s , String a) {
+        this.s = s;
+        s2 = a;
+    }
+
+    @Override
+    public int hashCode() {
+        int h = 31;
+        for (int i = 0; i < s.length(); i++) {
+            h = 31 * h + s.charAt(i);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            h = 31 * h + s2.charAt(i);
+        }
+        return h;
+    }
+}
